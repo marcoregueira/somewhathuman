3ef5372f0d6666f959fae4d5ec7ca3bd8d10dba1
Marco Regueira <marcoregueira@gmail.com>
2021-04-18 15:03:01 +0200

    debug

--- src/phyton/makehuman/run.sh
+++ src/phyton/makehuman/run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+
+
 cd /app
-gunicorn --config /etc/gunicorn/config.py app:app
+
+
+if [ "$FLASK_ENV" == "development" ]; then
+        python app.py
+else
+        gunicorn --config /etc/gunicorn/config.py app:app
+fi
